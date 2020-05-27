483610515b4d1e769d0b84f65307033d3f916693
Ezequiel Barreto <ezequielbrrt@gmail.com>
2020-05-27 12:11:16 -0500

    Web service layer

--- Reddit client/Services/WebService.swift
+++ Reddit client/Services/WebService.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct ResourceW<T> {
+    let url: URL
+    let parse: (Data) -> T?
+}
+
+final class WebService {
+   
+    func load<T>(resource: ResourceW<T>, completion: @escaping (T?) -> ()){
+        URLSession.shared.dataTask(with: resource.url){ data, response, error in
+                        
+            if let data = data{
+                DispatchQueue.main.async {
+                    completion(resource.parse(data))
+                }
+                
+            }else{
+                completion(nil)
+            }
+        }.resume()
+    }
+}
